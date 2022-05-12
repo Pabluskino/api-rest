91fb92e114e2413ede887777b5b4884fcd1c9f96
PabloUsuarioGit <pabluskino@gmail.com>
2022-05-12 22:30:49 +0200

    Con error en el loign

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const api = require('./routes')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.engine('.hbs', hbs({
+app.engine('.hbs', hbs.engine({
     defaultLayout: 'default',
     extname: '.hbs'
 }))
