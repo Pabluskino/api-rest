b172bc906db9d622a97494b65de9d9cef2666381
PabloUsuarioGit <pabluskino@gmail.com>
2022-05-19 19:24:58 +0200

    Sigue el error del login

--- controllers/user.js
+++ controllers/user.js
@@ -17,17 +17,32 @@ function signUp (req, res) {
     })
 }
 
-function signIn (req, res) {
-    User.find({ email: req.body.email }, (err, user) => {
-        if (err) return res.status(500).send({ message: err })
-        if (!user) return res.status(404).send({ message: 'No existe el usuario' })
+async function signIn (req, res) {
+    await User.findOne({ email: req.body.email })
+        .then((user) =>{
+            if(!user)
+                return res
+                    .status(404)
+                    .send({ message: "El usuario no está registrado" });
 
-        req.user = user
-        res.status(200).send({
-            message: 'Te has logueado correctamente',
-            token: service.createToken(user)
+            const password_verification = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+            if (password_verification) {
+                res.status(200).send({
+                    message: "Te has logeado correctamente",
+                    token: service.createToken(user),
+                });
+            } else {
+                res.status(500).send({ message: "Email o Contraseña incorrectos" });
+            }
         })
-    })
+        .catch((err) => {
+            return res
+                .status(500)
+                .send({ message: `Error al realizar la petición ${err}` });
+        });
 }
 
 module.exports = {
